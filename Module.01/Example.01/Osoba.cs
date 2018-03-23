fcef47ef93e5f23cc77a05269eb5f7dd5ae77a0b
Marcin Kruszyński (Macbook-Air) <github@kruszynscy.pl>
2018-03-23 13:41:25 +0100

    Przykład 1 - Tworzenie klasy

---
+++ Module.01/Example.01/Osoba.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+// Przykład 1 - tworzenie klasy
+
+namespace Example
+{
+    public class Osoba
+    {
+        public string Imie;
+        public string Nazwisko;
+        private int rokUrodzenia;
+
+        public Osoba()
+        {
+        }
+
+        public void UstawRokUrodzenia(int rokUrodzenia) {
+            if ((DateTime.Now.Year - rokUrodzenia) < 18) {
+                throw new ArgumentException("Osoba musi być pełnoletnia!");
+            }
+            this.rokUrodzenia = rokUrodzenia;
+        }
+
+        public string PobierzInformacje() {
+            string tytul = "";
+            if (Imie != null) {
+                if (czyKobieta()) {
+                    tytul = "Pani";
+                } else {
+                    tytul = "Pan";
+                }
+            }
+            return string.Format("{0} {1} {2} ur. w {3} roku.",
+                                 tytul, Imie, Nazwisko, rokUrodzenia);
+        }
+
+        private bool czyKobieta() {
+            if (Imie.EndsWith("a")) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Module.01/Example.01/Program.cs
+++ Module.01/Example.01/Program.cs
@@ -6,7 +6,25 @@ namespace Example._01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Osoba m = new Osoba();
+            Osoba k = new Osoba();
+
+            m.Nazwisko = "Kowalski";
+            m.Imie = "Jan";
+
+            k.Imie = "Anna";
+            k.Nazwisko = "Nowak";
+
+            m.UstawRokUrodzenia(1985);
+            k.UstawRokUrodzenia(1989);
+
+            Console.WriteLine("k - {0}", k.PobierzInformacje());
+            Console.WriteLine("m - {0}", m.PobierzInformacje());
+
+            Console.ReadKey();
+
+            Osoba dziecko = new Osoba();
+            dziecko.UstawRokUrodzenia(DateTime.Now.Year - 16); // <- osoba niepełnoletnia
         }
     }
 }
