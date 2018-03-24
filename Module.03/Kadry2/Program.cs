19498fa6a1f90f54979f9dcce4b0ceec2ef46229
Marcin Kruszyński (Macbook-Air) <github@kruszynscy.pl>
2018-03-24 11:07:16 +0100

    Zadanie 1 - Kadry(Wlasciwosci)

---
+++ Module.03/Kadry2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Kadry2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OsobaDlaKonsoli os1 = new OsobaDlaKonsoli();
+            Console.WriteLine();
+            os1.ZmienAdres();
+            Console.WriteLine();
+            os1.ZmienNazwisko();
+            Console.WriteLine("\n***************\n");
+            os1.WypiszOsobe();
+            Console.WriteLine("\n***************\n");
+
+            Osoba osoba2 = new Osoba(123, 1990, "Anna", "Kowalsa", 12, 23, "Kwiatowa", "97-350", "Piotrków Tryb.");
+            os1.Osoba = osoba2;
+            os1.ZmienAdres();
+            Console.WriteLine();
+            os1.ZmienNazwisko();
+            Console.WriteLine("\n***************\n");
+            os1.WypiszOsobe();
+
+            Console.ReadKey();
+        }
+    }
+}
